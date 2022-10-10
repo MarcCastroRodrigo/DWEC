32328acfd32a474716310480ae38f76c56ffbeb1
Marc <marc.cr.2003@gmail.com>
2022-10-10 13:17:34 +0200

    calc1

---
+++ CALCULADORA/js/index.js
@@ -0,0 +1,4 @@
+"use strict"
+alert ("Bienvenido a la calculadora");
+let operacion = prompt ("Introduce la operacion que quieres realizar (+,-,*,/)"); 
+let operadores = prompt ("Introduce los dos operadores separados por un espacio");
