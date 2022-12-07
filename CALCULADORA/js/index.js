2a2c44deb707cbe63095304c6fe03381b4daedca
Marc <marc.cr.2003@gmail.com>
2022-12-07 23:46:53 +0100

    calc2 corregida

--- CALCULADORA/js/index.js
+++ CALCULADORA/js/index.js
@@ -1,78 +1,72 @@
 "use strict"
 alert ("Bienvenido a la calculadora");
-let opCorrecta= true;
+
+let opCorrecta;
 let operacion;
-let calculadora = {
-    lastResult : 0
-};
-do{
-    function obtenerOperacion(){
-    do{
-        operacion = prompt ("Introduce la operacion que quieres realizar (+,-,*,/)"); 
-        switch(operacion){
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                break;   
-            default: alert("Operación incorrecta");
-                     !opCorrecta;    
-        }
-    }while(!opCorrecta);
-}
-obtenerOperacion();
+
+do {
+    function obtenerOperacion() {
+        do {
+            opCorrecta = true;
+            operacion = prompt ("Introduce la operacion que quieres realizar (+,-,*,/)");
+            if (operacion === null) {
+                break;
+            }
+            operacion = operacion.trim(); 
+            switch(operacion) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    break;   
+                default: opCorrecta = false; 
+                         alert("Operación incorrecta");                      
+            }
+        }while (!opCorrecta);
+    }
+    obtenerOperacion();
+    if (operacion === null) {
+        break;
+    }
     let operadores;
     let operador1;
     let operador2;
-    let obtenerOperandos = function(){
-        do{
+    let obtenerOperandos = function() {
+        do {
             operadores = prompt ("Introduce los dos operadores separados por un espacio");
-            operadores = operadores.trim();            
+            if (operadores === null) {
+                break;
+            }
+            operadores = operadores.trim();
             operador1 = Number(operadores.substring(0,operadores.indexOf(' ')));
             operador2 = Number(operadores.substring(operadores.indexOf(' '),operadores.length));
-            if (operadores.substring(0,operadores.indexOf(' ')) == "R"){
-                operador1 = calculadora.lastResult;
-               console.log(operador1);
-            }
-            if (operadores.substring(operadores.indexOf(' '),operadores.length).trim() == "R"){
-                operador2 = calculadora.lastResult;
-                console.log(operador2);
+            if (!operador1 || !operador2) {
+                alert("Introduce correctamente los 2 números separados por un espacio")
             }
         }while(!operador1 || !operador2);
     }
     obtenerOperandos();
-    calculadora.sumar = function() {
-        this.lastResult = operador1 + operador2;
-        return this.lastResult;
-            
-    }
-    calculadora.restar = function() {
-        this.lastResult = operador1 - operador2;
-        return this.lastResult;
-    }
-    calculadora.multiplicar = function() {
-        this.lastResult = operador1 * operador2;
-        return this.lastResult;
-    }
-    calculadora.dividir = function() {
-        this.lastResult = operador1 / operador2;
-        return this.lastResult;
-    }
-    switch(operacion){
-        case '+': alert(calculadora.sumar());
-                console.log(calculadora.lastResult);
+    if (operadores === null) {
+        break;
+    }   
+    let obtenerSuma = (operador1, operador2) => operador1 + operador2;
+    let obtenerResta = (operador1, operador2) => operador1 - operador2;
+    let obtenerProducto = (operador1, operador2) => operador1 * operador2;
+    let obtenerDivision = (operador1, operador2) => operador1 / operador2;
+    switch (operacion) {
+        case '+': 
+            alert(obtenerSuma(operador1, operador2));
             break;
-        case '-': alert(calculadora.restar());
+        case '-': 
+            alert(obtenerResta(operador1, operador2));
             break;
-        case '*': alert(calculadora.multiplicar());
+        case '*': 
+            alert(obtenerProducto(operador1, operador2));
             break;
-        case '/': alert(calculadora.dividir());
+        case '/': 
+            alert(obtenerDivision(operador1, operador2));
             break; 
     }
-}while(confirm("¿Quieres seguir operando?"));
-
-
-
-
+}while (confirm("¿Quieres seguir operando?"));
 
 
